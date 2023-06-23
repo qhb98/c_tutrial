1c204ac2bd118fa82e92a4cd06a9821d9b757c6d
LAPTOP-3EQTGSIT\26513 <2651311819@qq.com>
2023-06-23 23:13:37 +0800

    [add] 学习到指针

--- c++_start/start_chapters/demo11.cpp
+++ c++_start/start_chapters/demo11.cpp
@@ -11,17 +11,44 @@
 // 4. 函数体语句
 // 5. return表达式
 
+// 值传递 函数调用的时候函数的形参发生改变不会影响实参
+
+// 函数声明  告诉编译器函数的名称以及如何调用函数, 函数的实际主体可以单独定义
+// 函数的声明可以多次, 但是函数的定义只能有一次
+
 #include "iostream"
 
 using namespace std;
 
 // 子函数
+int add(int num1, int num2) {
+    int sum = 0;
+    sum = num1 + num2;
+    return sum;
+}
 
+void swap(int num1, int num2) {
+    cout << num1 << endl;
+    cout << num2 << endl;
+    int tmp_num = 0;
+    tmp_num = num1;
+    num1 = num2;
+    num2 = tmp_num;
+    cout << num1 << endl;
+    cout << num2 << endl;
+}
+
+int max(int a, int b);
+int max(int a);
+int max(int a, float b);
 
 
 // 主函数
 int main() {
+    int sum = add(1, 2);
+    cout << sum << endl;
 
+    swap(1, 2);
 
     system("pause");
     return 0;
